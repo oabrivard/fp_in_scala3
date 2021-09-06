64d91555f403a4ea32ad24e1618c67fbd238d71c
oabrivard <olivier@abrivard.fr>
2021-09-05 21:01:30 -0400

    Implement chapter 8.2.3 exercises

--- fpinscala3/src/main/scala/fpinscala3/chapter_8/Gen.scala
+++ fpinscala3/src/main/scala/fpinscala3/chapter_8/Gen.scala
@@ -1,8 +1,11 @@
 package fpinscala3.chapter_8
 
-import Gen._
-import Prop._
-import java.util.concurrent.{Executors,ExecutorService}
+import fpinscala3.chapter_6.*
+import fpinscala3.chapter_6.RNG.{flatMap, int, nonNegativeInt, sequenceWithFold, unit}
+import fpinscala3.chapter_8.Gen.*
+import fpinscala3.chapter_8.Prop.*
+
+import java.util.concurrent.{ExecutorService, Executors}
 
 /*
   sum: List[Int] => Int
@@ -33,16 +36,29 @@ object Prop:
   def forAll[A](gen: Gen[A])(f: A => Boolean): Prop = ???
 
 object Gen:
-  def unit[A](a: => A): Gen[A] = ???
+  /*
+  Implement Gen.choose using this representation of Gen. It should generate integers in the
+  range start to stopExclusive. Feel free to use functions you’ve already written.
+  */
+  def choose(start: Int, stopExclusive: Int): Gen[Int] = Gen(State {
+      RNG.map(RNG.nonNegativeInt) { n => start + n % (stopExclusive - start) }
+    })
 
+  def unit[A](a: => A): Gen[A] = Gen(State.unit(a))
+
+  def boolean: Gen[Boolean] = choose(0,2) map {(n:Int) => if n > 0 then true else false}
+
+  def listOfN[A](n: Int, g: Gen[A]): Gen[List[A]] =
+    val l = List.fill(n)(g.sample)
+    Gen(State.sequence(l))
+
+/*
 trait Gen[A]:
+  def map[A,B](f: A => B): Gen[B] = ???
+  def flatMap[A,B](f: A => Gen[B]): Gen[B] = ???
+*/
+case class Gen[A](sample: State[RNG,A]):
   def map[A,B](f: A => B): Gen[B] = ???
   def flatMap[A,B](f: A => Gen[B]): Gen[B] = ???
 
 trait SGen[+A]
-
-@main def hello: Unit =
-  println("Hello world!")
-  println(msg)
-
-def msg = "I was compiled by Scala 3. :)"
--- fpinscala3/src/test/scala/fpinscala3/chapter_8/GenTest.scala
+++ fpinscala3/src/test/scala/fpinscala3/chapter_8/GenTest.scala
@@ -1,8 +1,12 @@
-package fpinscala3.chapter_2
+package fpinscala3.chapter_8
 
+import fpinscala3.chapter_6.{RNG, SimpleRNG}
 import org.junit.Test
 import org.junit.Assert.*
 
 class GenTest:
-  @Test def t1(): Unit = 
-    assertEquals("msg", "msg")
+  @Test def ChooseTest(): Unit =
+    val rng1 = SimpleRNG(3107)
+    val gen = Gen.choose(10,100)
+    val (i,rng2) = gen.sample.run(rng1)
+    assertTrue(i >= 10 && i < 100)
