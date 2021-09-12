d6fed5b936d1dbe2f04cfb7309c547f4a43b07cd
oabrivard <olivier@abrivard.fr>
2021-09-12 17:46:06 -0400

    Implement chapter 8.4 exercises

--- fpinscala3/src/main/scala/fpinscala3/chapter_8/Gen.scala
+++ fpinscala3/src/main/scala/fpinscala3/chapter_8/Gen.scala
@@ -2,6 +2,7 @@ package fpinscala3.chapter_8
 
 import fpinscala3.chapter_6.*
 import fpinscala3.chapter_6.RNG.{flatMap, int, nonNegativeInt, sequenceWithFold, unit}
+import fpinscala3.chapter_7.Par
 import fpinscala3.chapter_8.Gen.*
 import fpinscala3.chapter_8.Prop.*
 
@@ -28,12 +29,11 @@ import java.util.concurrent.{ExecutorService, Executors}
 */
 
 enum Result(val isFalsified: Boolean):
-  case Passed extends Result(false)
   case Falsified(failure: FailedCase, successes: SuccessCount) extends Result(true)
+  case Passed extends Result(false)
+  case Proved extends Result(false)
 
 case class Prop(run: (MaxSize,TestCases,RNG) => Result):
-  def check: Boolean = ???
-
   def &&(p: Prop): Prop = Prop {(max,n,rng) => {
     val r = run(max,n,rng)
     if r.isFalsified then r else p.run(max,n,rng)
@@ -88,6 +88,65 @@ object Prop:
       prop.run(max,n,rng)
   }
 
+  def run(p: Prop,
+          maxSize: Int = 100,
+          testCases: Int = 100,
+          rng: RNG = SimpleRNG(System.currentTimeMillis)): Unit =
+    p.run(maxSize, testCases, rng) match
+      case Result.Falsified(msg, n) =>
+        println(s"! Falsified after $n passed tests:\n $msg")
+      case Result.Passed =>
+        println(s"+ OK, passed $testCases tests.")
+      case Result.Proved =>
+        println(s"+ OK, proved property.")
+
+  def check(p: => Boolean): Prop = Prop { (_, _, _) =>
+    if (p) Result.Passed else Result.Falsified("()", 0)
+  }
+
+  val ES: ExecutorService = Executors.newCachedThreadPool
+
+  val p1 = Prop.forAll(Gen.unit(Par.unit(1)))(i =>
+    Par.map(i)(_ + 1)(ES).get == Par.unit(2)(ES).get)
+
+  val p2 = check {
+    val p = Par.map(Par.unit(1))(_ + 1)
+    val p2 = Par.unit(2)
+    p(ES).get == p2(ES).get
+  }
+
+  def equal[A](p: Par[A], p2: Par[A]): Par[Boolean] =
+    Par.map2(p,p2)(_ == _)
+
+  val p3 = check {
+    equal (
+      Par.map(Par.unit(1))(_ + 1),
+      Par.unit(2)
+    )(ES).get
+  }
+
+  val S = weighted(
+    choose(1,4).map(Executors.newFixedThreadPool) -> .75,
+    Gen.unit(Executors.newCachedThreadPool) -> .25) // `a -> b` is syntax sugar for `(a,b)`
+
+  def forAllPar[A](g: Gen[A])(f: A => Par[Boolean]): Prop =
+    forAll(S ** g) { case (s,a) => f(a)(s).get }
+
+  def checkPar(p: Par[Boolean]): Prop = forAllPar(Gen.unit(()))(_ => p)
+
+  val p4 = checkPar {
+    equal (
+      Par.map(Par.unit(1))(_ + 1),
+      Par.unit(2)
+    )
+  }
+
+  val pint = Gen.choose(0,10) map (Par.unit(_))
+  val p5 = forAllPar(pint)(n => equal(Par.map(n)(y => y), n))
+
+  val p6 = forAllPar(pint)(n => equal(Par.fork(n), n))
+
+
 object Gen:
   /*
   Implement Gen.choose using this representation of Gen. It should generate integers in the
@@ -107,6 +166,14 @@ object Gen:
 
   def listOf[A](g: Gen[A]): SGen[List[A]] = SGen {listOfN(_,g)}
 
+  def weighted[A](g1: (Gen[A],Double), g2: (Gen[A],Double)): Gen[A] =
+    /* The probability we should pull from `g1`. */
+    val g1Threshold = g1._2.abs / (g1._2.abs + g2._2.abs)
+    Gen(State(RNG.double).flatMap(d => if (d < g1Threshold) g1._1.sample else g2._1.sample))
+
+  object **:
+    def unapply[A,B](p: (A,B)) = Some(p)
+
 /*
 trait Gen[A]:
   def map[A,B](f: A => B): Gen[B] = ???
@@ -114,6 +181,8 @@ trait Gen[A]:
 */
 case class Gen[+A](sample: State[RNG,A]):
   def map[B](f: A => B): Gen[B] = Gen(sample.map(f))
+
+  def map2[B,C](g: Gen[B])(f: (A,B) => C): Gen[C] = Gen(sample.map2(g.sample)(f))
 
   def flatMap[B](f: A => Gen[B]): Gen[B] = Gen(sample.flatMap(f(_).sample))
 
@@ -126,6 +195,8 @@ case class Gen[+A](sample: State[RNG,A]):
     choose(0,101) flatMap { (i:Int) => if (i > ratio.toInt) g2._1 else g1._1 }
 
   def unsized: SGen[A] = SGen { _ => this}
+
+  def **[B](g: Gen[B]): Gen[(A,B)] = (this map2 g)((_,_))
 
 case class SGen[+A](forSize: Int => Gen[A]):
   def apply(n: Int): Gen[A] = forSize(n)
--- fpinscala3/src/test/scala/fpinscala3/chapter_7/ParallelTest.scala
+++ fpinscala3/src/test/scala/fpinscala3/chapter_7/ParallelTest.scala
@@ -3,13 +3,17 @@ package fpinscala3.chapter_7
 import org.junit.Test
 import org.junit.Assert.*
 
+import java.util.concurrent.{ExecutorService, Executors}
+
 class ParallelTest:
   @Test def sum(): Unit =
+    val ES: ExecutorService = Executors.newCachedThreadPool
     val parSum = Par.sum(IndexedSeq(1,2,3,4))
-    val a = Par.run(Par.sequentialExecutor())(parSum)
+    val a = Par.run(ES)(parSum)
     assertEquals(10, a.get)
 
   @Test def max(): Unit =
+    val ES: ExecutorService = Executors.newCachedThreadPool
     val parMax = Par.max(IndexedSeq(1,2,7,4))
-    val a = Par.run(Par.sequentialExecutor())(parMax)
+    val a = Par.run(ES)(parMax)
     assertEquals(7, a.get)
